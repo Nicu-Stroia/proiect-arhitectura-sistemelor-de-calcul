1c5fd681bfea01d0670496f992f5dea30b8ffc10
SiLViU <silviuptc@gmail.com>
2026-04-21 20:32:59 +0300

    Refactor Changed name from Form1 to CPUViewer

--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,8 @@
 namespace proiect_arhitectura_sistemelor_de_calcul
 {
-    public partial class Form1 : Form
+    public partial class CPUViewerForm : Form
     {
-        public Form1()
+        public CPUViewerForm()
         {
             InitializeComponent();
         }
